0ec70b6eca95d605626ae003d1acb5bda9792405
Han <hanjisang0914@gmail.com>
2025-12-25 22:04:28 +0900

    default korean

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['ko', 'en'],
-  defaultLocale: 'en',
+  defaultLocale: 'ko',
   localeCookie: {
     name: 'NEXT_LOCALE',
     maxAge: 60 * 60 * 24 * 365  // 1 year persistence
